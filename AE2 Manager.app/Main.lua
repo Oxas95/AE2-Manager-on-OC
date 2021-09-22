810ba68430255158bfbd852b9404dc878b57977c
Oxas95 <32160224+Oxas95@users.noreply.github.com>
2021-09-22 23:38:46 +0200

    Update Main.lua

--- AE2 Manager.app/Main.lua
+++ AE2 Manager.app/Main.lua
@@ -6,6 +6,8 @@ local sides = require("sides")
 local os = require("os")
 local cpus = nil
 os.execute("Strings")
+demandes = nil
+
 
 local args = {...}
 
@@ -32,12 +34,26 @@ local function getAvailableCpu()
   return nil
 end
 
+local function requestCraft(demande)
+  for i = 1, #cpus do
+    if cpus[i].busy == false then
+      --print("cpu ".. i .. " is free")
+      demande.craft.request(demande.craftCount,false,cpus[i].name)
+      if cpus[i].busy == true then
+        print("create request for " .. demande.craftCount .. " " .. demande.craft.getItemStack().label .. " using cpu " .. cpu[i].name)
+        return
+      end
+    end
+  end
+end
+
 local function parseExp(demande)
   success, value = tonumber(demande.exp)
   if success == true then
     demande.craftCount = value
   else
     str = string.gsub(demande.exp, "n", tostring(demande.item.size))
+    str = string.gsub(str, "m", tostring(demande.craft.getItemStack().size))
     func = assert(load("return " .. str))
     demande.craftCount = func()
     if demande.craftCount % 1 ~= 0 then
@@ -61,11 +77,16 @@ end
 
 local function getCraft(demandes)
   craft = me.getCraftables()
+  item = {}
+  for i = 1, #craft do
+    item[i] = craft[i].getItemStack()
+  end
   for j = 1, #demandes do
     for i = 1, #craft do
-      itemStack = craft[i].getItemStack()
-      if itemStack.name == demandes[j].toCraft and itemStack.damage == demandes[j].damage2 then
+      if item[i].name == demandes[j].toCraft and item[i].damage == demandes[j].damage2 then
+        print(j .. " select the craft " .. i)
         demandes[j].craft = craft[i]
+        i = #craft + 1
       end
     end
   end
@@ -75,10 +96,9 @@ local function refreshCpus()
   cpus = me.getCpus()
 end
 
-local function refreshData(demandes)
-  cpus = me.getCpus()
+function refreshData(demandes)
+  refreshCpus()
   getItem(demandes)
-  getCraft(demandes)
 end
 
 local function readConfig(path)
@@ -98,70 +118,79 @@ end
 
 local function traiteDemande(demande)
   if demande.craft ~= nil and demande.item ~= nil then
-    cpu = getAvailableCpu()
+    cpu = getAvailableCpu(demande.craftCount)
     if demande.comparator == "<" then
       if demande.item.size < demande.count then
-        if cpu ~= nil then 
-          print("create request for " .. demande.craftCount .. " " .. demande.toCraft)
-          demande.craft.request(demande.craftCount,false,cpu.name)
+        if cpu ~= nil then
+          print("request for " .. demande.craft.getItemStack().label)
+          requestCraft(demande)
         end
       end
     elseif demande.comparator == ">" then
       if demande.item.size > demande.count then
         if cpu ~= nil then
-          print("create request for " .. demande.craftCount .. " " .. demande.toCraft)
-          demande.craft.request(demande.craftCount,false,cpu.name)
+          print("request for " .. demande.craft.getItemStack().label)
+          requestCraft(demande)
         end
       end
     elseif demande.comparator == "<=" then
       if demande.item.size <= demande.count then
         if cpu ~= nil then 
-          print("create request for " .. demande.craftCount .. " " .. demande.toCraft)
-          demande.craft.request(demande.craftCount,false,cpu.name)
+          print("request for " .. demande.craft.getItemStack().label)
+          requestCraft(demande)
         end
       end
     elseif demande.comparator == ">=" then
       if demande.item.size >= demande.count then
         if cpu ~= nil then 
-          print("create request for " .. demande.craftCount .. " " .. demande.toCraft)
-          demande.craft.request(demande.craftCount,false,cpu.name)
+          print("request for " .. demande.craft.getItemStack().label)
+          requestCraft(demande)
         end
       end
     elseif demande.comparator == "==" then
       if demande.item.size == demande.count then
-        if cpu ~= nil then 
-          print("create request for " .. demande.craftCount .. " " .. demande.toCraft)
-          demande.craft.request(demande.craftCount,false,cpu.name)
+        if cpu ~= nil then
+          print("request for " .. demande.craft.getItemStack().label) 
+          requestCraft(demande)
         end
       end
     elseif demande.comparator == "!=" then
       if demande.item.size ~= demande.count then
         if cpu ~= nil then 
-          print("create request for " .. demande.craftCount .. " " .. demande.toCraft)
-          demande.craft.request(demande.craftCount,false,cpu.name)
+          print("request for " .. demande.craft.getItemStack().label)
+          requestCraft(demande)
         end
       end
     else
       print("Error : invalid Comparator")
       os.exit()
     end
+  else
+    return false
   end
-  return nil
+  return true
 end
 
 local function run(demandes)
   refreshData(demandes)
   for i = 1, #demandes do
-    traiteDemande(demandes[i])
+    ret = traiteDemande(demandes[i])
+    if ret == false then
+      getCraft(demandes)
+    end
   end
 end
 
 local function main()
   demandes = readConfig("config.cfg")
+  side = sides.back
   if #demandes ~= 0 then
-    while redstone.getInput(sides.left) > 0 do
-      os.sleep(1)
+    if redstone.getInput(side) > 0 then
+      getCraft(demandes)
+    end
+    while redstone.getInput(side) > 0 do
       run(demandes)
+      os.sleep(60)
     end
   end
   strings = nil
